b169e7a55be00fa19c5a3869feb3e60414fa17c6
DorinGalben <91731551+DorinGalben@users.noreply.github.com>
2021-12-14 13:58:41 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Sandbox_Winforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 128);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(679, 512);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(393, 109);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "number+number=puzzle";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(411, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Solve";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(411, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Translate input";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(560, 87);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Generate random";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(560, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "8";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(582, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Max no. digits";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 652);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,425 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sandbox_Winforms
+{
+    public partial class Form1 : Form
+    {
+        private List<string> words = new List<string>();
+
+        private List<string> words_reversed = new List<string>();
+
+        HashSet<char> letters = new HashSet<char>();
+
+        private string jessTemplatePath = Directory.GetCurrentDirectory() + "\\jessTemplate.txt";
+
+        int wordMaxLength = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Saving the words && the solution
+
+            wordMaxLength = 0; words = new List<string>(); words_reversed = new List<string>(); letters = new HashSet<char>();
+                       
+            char[] separators = new char[] { '+', '=' };
+
+            var tokens = richTextBox2.Text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            for(int i=0;i<tokens.Length;i++)
+            {
+                words.Add(tokens[i].Trim().ToUpper());
+
+                wordMaxLength = System.Math.Max(words[i].Length, wordMaxLength); //finding the maximum string length of all words
+            }
+
+            //Generating reverse words and adding them in new list
+            for (int i = 0; i < words.Count(); i++)
+            {
+                String word_reversed = "";
+
+                for(int j=words[i].Length-1;j>=0;j--)
+                {
+                    word_reversed += words[i][j];
+
+                    if(!letters.Contains(words[i][j]))
+                    {
+                        letters.Add(words[i][j]);
+                    }
+                }
+
+                words_reversed.Add(word_reversed);
+            }
+
+            string jessTemplate = "";
+
+            using (StreamReader reader = new StreamReader(File.OpenRead(jessTemplatePath)))
+            {
+                jessTemplate = reader.ReadToEnd();
+            }        
+
+            jessTemplate=jessTemplate.Replace("{comment_problemInput}", generate_commentProblemInput());
+
+            jessTemplate = jessTemplate.Replace("{startup_printOut}", generate_startupPrintout());
+
+            jessTemplate = jessTemplate.Replace("{startup_letters}", generate_startupLetters());
+
+            jessTemplate = jessTemplate.Replace("{rule_distinctLetters}", generate_ruleDistinctLetters());
+
+            jessTemplate = jessTemplate.Replace("{rule_additionOperations}", generate_ruleAdditionOperations());
+
+            jessTemplate = jessTemplate.Replace("{printout_solution1}", generate_printoutSolution1());
+
+            jessTemplate = jessTemplate.Replace("{printout_solution2}", generate_printoutSolution2());
+
+            richTextBox1.Text = jessTemplate;
+        }
+
+        string generate_printoutSolution1()
+        {
+            string result = "";
+
+            var orderedLetters = letters.OrderBy(p => p);
+
+            foreach(var letter in orderedLetters)
+            {
+                result += string.Format("(printout t \"  {0} = \" ?{1} )", Char.ToUpper(letter), Char.ToLower(letter)) + Environment.NewLine;
+            }
+
+            return result;
+
+        }
+
+        string generate_printoutSolution2()
+        {
+            string result = "";
+
+            int bufferLength = wordMaxLength + 2;
+
+            for(int i=0;i<words.Count;i++)
+            {
+                int length_word = words[i].Length;
+
+                int decorator_length = bufferLength - length_word;
+
+                string decorator_sign = "+";
+
+                string wordVariable = "";
+
+                if(i==0)
+                {
+                    decorator_sign = " ";
+                }
+
+                if(i==words.Count-1)
+                {
+                    result += string.Format("(printout t \"{0}\" \"{1}\" crlf)", decorator_emptyString(bufferLength - wordMaxLength-1), decorator_dashedLine(wordMaxLength+1)) + Environment.NewLine;
+                    decorator_sign = "=";
+                }
+
+                for(int j=0;j< words[i].Length;j++)
+                {
+                    wordVariable += string.Format(" ?{0}", Char.ToLower(words[i][j]));
+                }
+
+                result += string.Format("(printout t \"{0}{1}\" {2} crlf)", decorator_emptyString(decorator_length), decorator_sign, wordVariable) + Environment.NewLine;
+            }
+
+            var orderedLetters = letters.OrderBy(p => p);
+
+            return result;
+
+        }
+        string generate_commentProblemInput()
+        {
+            int bufferLength = wordMaxLength + 7;
+
+            string comment_problemInput = Environment.NewLine + bufferedString("", bufferLength, true) + Environment.NewLine;
+
+            string decorator = "";
+
+            for (int i = 0; i < words.Count - 1; i++)
+            {
+                if (i != 0) decorator = "+";
+
+                comment_problemInput += bufferedString(decorator + words[i], bufferLength, true) + Environment.NewLine;
+            }
+
+            comment_problemInput += bufferedString(decorator_dashedLine(wordMaxLength), bufferLength, true) + Environment.NewLine;
+
+            comment_problemInput += bufferedString("="+words[words.Count - 1], bufferLength, true) + Environment.NewLine;
+
+            comment_problemInput += bufferedString("", bufferLength, true) + Environment.NewLine;
+
+            return comment_problemInput;
+        }
+
+        string generate_ruleAdditionOperations()
+        {
+            string result = "";
+            string lastWord = words_reversed[words_reversed.Count - 1];
+            string l1, l2, l3;
+            string l1_prev, l2_prev, l3_prev;
+
+            l1 = l2 = l3 = "0";
+
+            for(int i=0;i<wordMaxLength;i++)
+            {
+                l1_prev = l1;l2_prev = l2;l3_prev = l3;
+
+                l1 = l2 = l3 = "0";
+
+                if(i<lastWord.Length)
+                {
+                    l1 = "?" + Char.ToLower(lastWord[i]);
+                }
+
+                if(i<words_reversed[0].Length)
+                {
+                    l2 = "?" + Char.ToLower(words_reversed[0][i]);
+                }
+
+                if (i < words_reversed[1].Length)
+                {
+                    l3 = "?" + Char.ToLower(words_reversed[1][i]);
+                }
+
+                if( l1 !=l2 && l3=="0" &&(l2_prev=="0" || l3_prev=="0"))
+                {
+                    l3 = "1";
+                }
+
+                if (i==wordMaxLength-1)
+                {
+                    result += string.Format("(combination (letter {0}) (number {1}&:(<  (+ (+ {2} {3}) (/ (+ {4} {5}) 10))  10)))", l1.Substring(1).ToUpper(), l1, l2, l3, l2_prev, l3_prev, l1) + Environment.NewLine;
+                }
+
+                result += string.Format("(combination (letter {0}) (number {1}&:(= (mod (+ (+ {2} {3}) (/ (+ {4} {5}) 10)) 10) {6}))) ",l1.Substring(1).ToUpper(),l1,l2,l3,l2_prev,l3_prev,l1) + Environment.NewLine;
+            }
+
+            return result;
+        }
+
+        string generate_ruleDistinctLetters()
+        {
+            string result = "";
+
+            var orderedLetters = letters.OrderBy(p => p);
+
+            for(int i=0;i<orderedLetters.Count();i++)
+            {
+                char letter = orderedLetters.ElementAt(i);
+
+                string decorator_difference = "";
+
+                for(int j=0;j<i;j++)
+                {
+                    decorator_difference += "&~?" + Char.ToLower(orderedLetters.ElementAt(j));
+                }
+
+                result += string.Format("(combination (letter {0}) (number ?{1}{2}))", Char.ToUpper(letter), Char.ToLower(letter), decorator_difference) + Environment.NewLine;
+            }
+
+            return result;
+        }
+
+        string generate_startupLetters()
+        {
+            string result = "";
+
+            foreach(char letter in letters.OrderBy(p=>p))
+            {
+                result += (string.Format("(letter {0})", Char.ToUpper(letter)));
+            }
+
+            return result;
+        }
+
+        string generate_startupPrintout()
+        {
+            int bufferLength = wordMaxLength + 3;
+
+            string result = "";
+
+            result +=string.Format("(printout t \"{0}\" crlf)", "The problem is") + Environment.NewLine;
+
+            string decorator = "";
+
+            for (int i = 0; i < words.Count - 1; i++)
+            {
+                if (i != 0) decorator = "+";
+
+                result += string.Format("(printout t \"{0}\" crlf)", bufferedString(decorator + words[i], bufferLength)) + Environment.NewLine;
+            }
+
+            result += string.Format("(printout t \"{0}\" crlf)", bufferedString(decorator_dashedLine(wordMaxLength), bufferLength)) + Environment.NewLine;
+
+            result += string.Format("(printout t \"{0}\" crlf)", bufferedString("=" + words[words.Count - 1], bufferLength)) + Environment.NewLine;           
+
+            return result;
+        }
+
+        string bufferedString(string str, int bufferLength, bool addComments=false)
+        {
+            StringBuilder bufferedStr = new StringBuilder();
+
+            for(int i=0;i<bufferLength;i++)
+            {
+                bufferedStr.Append(" ");
+            }
+
+            int contor = 0;
+
+            while(contor<str.Length && contor<bufferLength-1)
+            {
+                bufferedStr[bufferLength - 1 - contor] = str[str.Length - 1 - contor];
+
+                contor++;
+            }
+
+            if(addComments)
+            {
+                for (int i = 0; i < 3; i++)
+                    bufferedStr[i] = ';';
+            }
+
+            return bufferedStr.ToString();
+        }
+
+
+        string decorator_dashedLine(int length)
+        {
+            String str = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                str += "-";
+            }
+
+            return str;
+        }
+
+        string decorator_emptyString(int length)
+        {
+            String str = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                str += " ";
+            }
+
+            return str;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Dictionary<char, char> dictionary = new Dictionary<char, char>();
+
+            Random rand = new Random();
+
+            int maxValue = 'z' - 'a';
+
+            for(int i=0;i<=9;i++)
+            {
+                char value = (char)('A' + rand.Next(0, maxValue));
+
+                dictionary.Add(i.ToString()[0], value);
+            }
+
+            string result = richTextBox2.Text;
+
+            if (!result.Contains('='))
+            {
+                char[] separators = new char[] { '+', '=', ' ' };
+
+                var tokens = richTextBox2.Text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                Int64 sum = 0;
+
+                foreach (var token in tokens)
+                {
+                    try
+                    {
+                        sum += Convert.ToInt64(token);
+                    }
+                    catch(Exception ex) { }
+                }
+
+                result += " = " + sum;
+
+                richTextBox2.Text = result;
+            }
+
+            for (int i=0;i<=9;i++)
+            {
+                result = result.Replace(i.ToString()[0], dictionary[i.ToString()[0]]);
+            }
+
+            richTextBox1.Text = result;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int max_noDigits = 8;
+
+            try
+            {
+                max_noDigits = Convert.ToInt32(textBox1.Text);
+            }
+            catch(Exception ex) { }
+
+            Random rand = new Random();
+
+
+            int nr_digits;
+            string nr_1 = "", nr_2 = "";
+
+            nr_digits = rand.Next(3, max_noDigits);
+            
+            //first digit shouldn't be 0
+            nr_1 += (char)('0' + rand.Next(1, 9));
+
+            //adding rest of the digits
+            for (int i = 1; i < nr_digits; i++)
+                nr_1 += (char)('0' + rand.Next(0, 9));
+
+
+            //first digit shouldn't be 0
+            nr_2 += (char)('0' + rand.Next(1, 9));
+
+            //adding rest of the digits
+            for (int i = 1; i < nr_digits; i++)
+                nr_2 += (char)('0' + rand.Next(0, 9));
+
+            if(nr_1.Length<nr_2.Length)
+            {
+                string aux = nr_1;
+                nr_1 = nr_2;
+                nr_2 = aux;
+            }
+            richTextBox2.Text = nr_1 + " + " + nr_2;
+
+            button2_Click(null, null);
+        }
+    }
+
+
+}
